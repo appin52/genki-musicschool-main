ac46e9dead6fb14f27eac423f57e80151849de50
orange5dog3 <orange5dog3@gmail.com>
2025-08-25 06:13:52 +0900

    パンクズ完成

--- template-parts/breadcrumbs.php
+++ template-parts/breadcrumbs.php
@@ -1,7 +1,9 @@
 <div class="c-band">
     <div class="inner">
-        <?php if (function_exists('bcn_display')) {
-            bcn_display();
-        } ?>
+        <div class="c-band-text">
+            <?php if (function_exists('bcn_display')) {
+                bcn_display();
+            } ?>
+        </div>
     </div>
 </div>
